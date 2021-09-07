5c442d44f62d9246d61bf3984b8728c73921d059
Lucas Vinicius <lucass.lv.lv@gmail.com>
2021-09-07 11:27:34 -0300

    função de autenticação e função para geração de token

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,7 +1,21 @@
 const User = require('../models/User');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const authConfig = require('../config/auth');
+
+function tokenCreator(id) {
+  // informa o dado que nunca vai se repetir,
+  // hash da aplicação,
+  // data de expiração do token (sec)
+  const token = jwt.sign(id, authConfig.secret, {
+    expiresIn: 86400,
+  });
+
+  return token;
+}
 
 module.exports = {
-  // Rota /register
+  // Rota: /register
   async register(req, res) {
     const { email } = req.body;
 
@@ -10,14 +24,31 @@ module.exports = {
       if (await User.findOne({ email })) {
         return res.status(400).send({ error: 'User alredy exists' });
       }
-      const user = await User.create(req.body);
-      user.password = undefined;
 
-      return res.send({ user });
+      const user = await User.create(req.body);
+      user.password = undefined; //faz com que a senha não seja retornada após o create
+      const token = tokenCreator({ id: user._id });
+
+      return res.send({ user, token });
     } catch (err) {
       return res.status(400).send({ error: 'Registration failed' });
     }
   },
 
-  // Rota /
+  // Rota: /autheticate
+  async autheticate(req, res) {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email }).select('+password'); // busca uma propriedade select=false
+
+    // validação de login e senha
+    if (!user) {
+      return res.status(400).send({ error: 'User not found' });
+    } else if (!(await bcrypt.compare(password, user.password))) {
+      return res.status(400).send({ error: 'Invalid password' });
+    }
+    user.password = undefined;
+    const token = tokenCreator({ id: user._id });
+
+    return res.send({ user, token });
+  },
 };
