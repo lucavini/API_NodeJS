85a30b741668d44210bf944d20372e0316875c35
Lucas Vinicius <lucass.lv.lv@gmail.com>
2021-09-15 09:59:06 -0300

    rotas /forgot_password e / projects

--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,16 @@ const routes = express.Router();
 
 //Controllers
 const authController = require('./controllers/authController');
+const projectController = require('./controllers/projectController');
 
 // Rota /auth
 routes.post('/auth/register', authController.register);
 routes.post('/auth/autheticate', authController.autheticate);
+routes.post('/auth/forgot_password', authController.forgot_password);
 
-// exporta as rotas definidas
+// Requisições protegidas via middlewares
+// Rota /projects
+routes.get('/projects',projectController.projects);
+
+
 module.exports = routes;
